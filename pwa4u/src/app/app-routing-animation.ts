c15bbe40e277e0b14cd77315650573f9833fc464
dmatejka <matejka@xpression.sk>
2018-06-21 13:41:02 +0200

    refactoring HOME MENU animation

--- pwa4u/src/app/app-routing-animation.ts
+++ pwa4u/src/app/app-routing-animation.ts
@@ -67,38 +67,32 @@ export const homeTransition = transition('*=>home', [
     ),
   ]),
   query(
-    '.container div span',
-    stagger('100ms', [
+    '.container div ',
+    stagger('250ms', [
       animate(
-        '450ms',
+        '650ms',
         keyframes([
-          style({ opacity: 0, offset: 0.0 }),
+          style({ opacity: 0.8, offset: 0.0 }),
           style({ opacity: 0.6, offset: 0.1 }),
           style({ opacity: 1, offset: 0.101 }),
-          style({ opacity: 0, offset: 0.103 }),
           style({ opacity: 0.6, offset: 0.2 }),
           style({ opacity: 1, offset: 0.202 }),
-          style({ opacity: 0, offset: 0.205 }),
-          style({ opacity: 0.6, offset: 0.3 }),
+          style({ opacity: 0.8, offset: 0.205 }),
           style({ opacity: 1, offset: 0.305 }),
-          style({ opacity: 0, offset: 0.308 }),
+          style({ opacity: 0.8, offset: 0.308 }),
           style({ opacity: 0.6, offset: 0.4 }),
           style({ opacity: 1, offset: 0.405 }),
-          style({ opacity: 0, offset: 0.5 }),
           style({ opacity: 0.6, offset: 0.502 }),
           style({ opacity: 1, offset: 0.508 }),
-          style({ opacity: 0, offset: 0.6 }),
-          style({ opacity: 0.6, offset: 0.605 }),
+          style({ opacity: 0.8, offset: 0.6 }),
           style({ opacity: 1, offset: 0.7 }),
-          style({ opacity: 0, offset: 0.702 }),
-          style({ opacity: 0.6, offset: 0.708 }),
+          style({ opacity: 0.8, offset: 0.702 }),
           style({ opacity: 1, offset: 0.8 }),
-          style({ opacity: 0, offset: 0.802 }),
-          style({ opacity: 0.6, offset: 0.808 }),
+          style({ opacity: 0.8, offset: 0.802 }),
           style({ opacity: 1, offset: 0.9 }),
-          style({ opacity: 0, offset: 0.902 }),
+          style({ opacity: 0.8, offset: 0.902 }),
           style({ opacity: 1, offset: 0.908 }),
-          style({ opacity: 0.5, offset: 1 }),
+          // style({ opacity: 0.9, offset: 1 }),
         ])
       ),
     ]),
@@ -109,14 +103,22 @@ export const homeTransition = transition('*=>home', [
 // HOME TO UX Transition
 export const homeToUXTransition = transition('home=>UX', [
   group([
+    /* SUBTITLE animation */
+    query('#subtitle', [
+      animate('400ms', style({ opacity: 0 })),
+      style({ opacity: 0 }),
+    ]),
+
+    /* BOXES animation */
     query('.container div', [
-      style({ 'border-width': 0 }),
       animate('200ms', style({ opacity: 0 })),
       style({ opacity: 0 }),
     ]),
+
     query('.UX', [
       style({
         opacity: 1,
+        'background-color': 'var(--darker-UX-color)',
         'transform-origin': '50% 50%',
       }),
       group([
@@ -135,13 +137,11 @@ export const homeToUXTransition = transition('home=>UX', [
       ]),
     ]),
 
+    /* BACKGROIUND CIRCLE animation */
     query('.limit ', [
       style({
         position: 'absolute',
-        top: 0,
-        left: 0,
-        overflow: 'hidden',
-        'z-index': 5,
+        opacity: 1,
         width: '100vw',
         height: '100vh',
       }),
@@ -149,15 +149,7 @@ export const homeToUXTransition = transition('home=>UX', [
 
     query('.backgroudcircle ', [
       style({
-        position: 'relative',
-        'transform-origin': '50 50',
-        left: '50vw',
-        top: '50vh',
-        opacity: 0,
-        'border-radius': '50%',
         'background-color': 'var(--darker-UX-color)',
-        width: '1em',
-        height: '1em',
       }),
       animate(
         '750ms ease-out',
@@ -173,14 +165,22 @@ export const homeToUXTransition = transition('home=>UX', [
 // HOME TO FE Transition
 export const homeToFETransition = transition('home=>FE', [
   group([
+    /* SUBTITLE animation */
+    query('#subtitle', [
+      animate('400ms', style({ opacity: 0 })),
+      style({ opacity: 0 }),
+    ]),
+
+    /* BOXES animation */
     query('.container div', [
-      style({ 'border-width': 0 }),
       animate('200ms', style({ opacity: 0 })),
       style({ opacity: 0 }),
     ]),
+
     query('.FE', [
       style({
         opacity: 1,
+        'background-color': 'var(--darker-FE-color)',
         'transform-origin': '50% 50%',
       }),
       group([
@@ -199,13 +199,11 @@ export const homeToFETransition = transition('home=>FE', [
       ]),
     ]),
 
+    /* BACKGROIUND CIRCLE animation */
     query('.limit ', [
       style({
         position: 'absolute',
-        top: 0,
-        left: 0,
-        overflow: 'hidden',
-        'z-index': 5,
+        opacity: 1,
         width: '100vw',
         height: '100vh',
       }),
@@ -213,15 +211,7 @@ export const homeToFETransition = transition('home=>FE', [
 
     query('.backgroudcircle ', [
       style({
-        position: 'relative',
-        'transform-origin': '50 50',
-        left: '50vw',
-        top: '50vh',
-        opacity: 0,
-        'border-radius': '50%',
         'background-color': 'var(--darker-FE-color)',
-        width: '1em',
-        height: '1em',
       }),
       animate(
         '750ms ease-out',
@@ -237,14 +227,22 @@ export const homeToFETransition = transition('home=>FE', [
 // HOME TO SL Transition
 export const homeToSLTransition = transition('home=>SL', [
   group([
+    /* SUBTITLE animation */
+    query('#subtitle', [
+      animate('400ms', style({ opacity: 0 })),
+      style({ opacity: 0 }),
+    ]),
+
+    /* BOXES animation */
     query('.container div', [
-      style({ 'border-width': 0 }),
       animate('200ms', style({ opacity: 0 })),
       style({ opacity: 0 }),
     ]),
+
     query('.SL', [
       style({
         opacity: 1,
+        'background-color': 'var(--darker-SL-color)',
         'transform-origin': '50% 50%',
       }),
       group([
@@ -263,13 +261,11 @@ export const homeToSLTransition = transition('home=>SL', [
       ]),
     ]),
 
+    /* BACKGROIUND CIRCLE animation */
     query('.limit ', [
       style({
         position: 'absolute',
-        top: 0,
-        left: 0,
-        overflow: 'hidden',
-        'z-index': 5,
+        opacity: 1,
         width: '100vw',
         height: '100vh',
       }),
@@ -277,15 +273,7 @@ export const homeToSLTransition = transition('home=>SL', [
 
     query('.backgroudcircle ', [
       style({
-        position: 'relative',
-        'transform-origin': '50 50',
-        left: '50vw',
-        top: '50vh',
-        opacity: 0,
-        'border-radius': '50%',
         'background-color': 'var(--darker-SL-color)',
-        width: '1em',
-        height: '1em',
       }),
       animate(
         '750ms ease-out',
@@ -301,14 +289,22 @@ export const homeToSLTransition = transition('home=>SL', [
 // HOME TO BE Transition
 export const homeToBETransition = transition('home=>BE', [
   group([
+    /* SUBTITLE animation */
+    query('#subtitle', [
+      animate('400ms', style({ opacity: 0 })),
+      style({ opacity: 0 }),
+    ]),
+
+    /* BOXES animation */
     query('.container div', [
-      style({ 'border-width': 0 }),
       animate('200ms', style({ opacity: 0 })),
       style({ opacity: 0 }),
     ]),
+
     query('.BE', [
       style({
         opacity: 1,
+        'background-color': 'var(--darker-BE-color)',
         'transform-origin': '50% 50%',
       }),
       group([
@@ -327,13 +323,11 @@ export const homeToBETransition = transition('home=>BE', [
       ]),
     ]),
 
+    /* BACKGROIUND CIRCLE animation */
     query('.limit ', [
       style({
         position: 'absolute',
-        top: 0,
-        left: 0,
-        overflow: 'hidden',
-        'z-index': 5,
+        opacity: 1,
         width: '100vw',
         height: '100vh',
       }),
@@ -341,15 +335,7 @@ export const homeToBETransition = transition('home=>BE', [
 
     query('.backgroudcircle ', [
       style({
-        position: 'relative',
-        'transform-origin': '50 50',
-        left: '50vw',
-        top: '50vh',
-        opacity: 0,
-        'border-radius': '50%',
         'background-color': 'var(--darker-BE-color)',
-        width: '1em',
-        height: '1em',
       }),
       animate(
         '750ms ease-out',
