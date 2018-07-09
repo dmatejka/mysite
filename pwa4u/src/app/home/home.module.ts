c51ccfb52fe43145234dc0250eac5601dbbc185a
dmatejka <matejka@xpression.sk>
2018-07-09 23:30:18 +0200

    responsive STATE chooser with swipe

--- pwa4u/src/app/home/home.module.ts
+++ pwa4u/src/app/home/home.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ResponsiveNameComponent } from './responsive-name/responsive-name.component';
+import { ResponsivePictureComponent } from './responsive-picture/responsive-picture.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
-  declarations: [HomepageComponent, ResponsiveNameComponent]
+  declarations: [HomepageComponent, ResponsiveNameComponent, ResponsivePictureComponent]
 })
 export class HomeModule { }
--- pwa4u/src/app/home/responsive-name/responsive-name.component.ts
+++ pwa4u/src/app/home/responsive-name/responsive-name.component.ts
@@ -5,7 +5,7 @@ import {
   transition,
   trigger,
 } from '@angular/animations';
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, HostListener, Input, OnInit, Output } from '@angular/core';
 import { EventEmitter } from 'events';
 
 type ResponsiveState = 'desktop' | 'mobile' | 'tablet';
@@ -15,21 +15,21 @@ type ResponsiveState = 'desktop' | 'mobile' | 'tablet';
   templateUrl: './responsive-name.component.html',
   styleUrls: ['./responsive-name.component.scss'],
   animations: [
-    trigger('responsiveState', [
+    trigger('responsiveNameState', [
       state(
         'inactiveL',
         style({
-          filter: 'blur(2px)',
+          filter: 'blur(1px)',
           opacity: 0.7,
-          transform: 'translateX(-8em) scale(1)',
+          transform: 'translateX(-8em)',
         })
       ),
       state(
         'inactiveR',
         style({
-          filter: 'blur(2px)',
+          filter: 'blur(1px)',
           opacity: 0.7,
-          transform: 'translateX(3em) scale(1)',
+          transform: 'translateX(3em)',
         })
       ),
       state(
@@ -37,19 +37,52 @@ type ResponsiveState = 'desktop' | 'mobile' | 'tablet';
         style({
           filter: 'blur(0)',
           opacity: 1,
-          transform: 'translateX(-2.5em) scale(1)',
+          transform: 'translateX(-2.5em)',
         })
       ),
 
-      transition('inactiveL <=> inactiveR', [
+      transition('inactiveL => inactiveR', [
         animate(
           250,
           style({
-            transform: 'scale(0)',
+            // transform: 'scale(0)',
+            transform: 'translateX(-13.5em)',
             opacity: 0,
           })
         ),
       ]),
+      transition('inactiveR => inactiveL', [
+        animate(
+          250,
+          style({
+            // transform: 'scale(0)',
+            transform: 'translateX(8.5em)',
+            opacity: 0,
+          })
+        ),
+      ]),
+
+      transition('active <=> *', [animate(350)]),
+    ]),
+
+    trigger('responsiveIconState', [
+      state(
+        'inactive',
+        style({
+          filter: 'blur(3px)',
+          opacity: 0,
+          transform: 'translateX(-50%) scale(0)',
+        })
+      ),
+
+      state(
+        'active',
+        style({
+          filter: 'blur(0)',
+          opacity: 1,
+          transform: 'translateX(-50%) scale(1)',
+        })
+      ),
 
       transition('active <=> *', [animate(350)]),
     ]),
@@ -59,9 +92,27 @@ export class ResponsiveNameComponent implements OnInit {
   @Input() state: ResponsiveState = 'tablet';
   @Output() ResponsiveStateEmitted = new EventEmitter();
 
+  responsiveNames: Array<ResponsiveState> = ['desktop', 'tablet', 'mobile'];
   constructor() {}
 
   ngOnInit() {}
+
+  @HostListener('swiperight', ['$event.type'])
+  @HostListener('swipeleft', ['$event.type'])
+  swipe(e) {
+    console.log('inner swipe', { e });
+    const currentIdx = this.responsiveNames.indexOf(this.state);
+    console.log('currentIdx', currentIdx, this.responsiveNames[currentIdx]);
+    let newIdx = currentIdx;
+    if (e === 'swiperight') {
+      newIdx = currentIdx - 1 >= 0 ? currentIdx - 1 : 2;
+    }
+    if (e === 'swipeleft') {
+      newIdx = currentIdx + 1 < 3 ? currentIdx + 1 : 0;
+    }
+    console.log('newIdx', newIdx, this.responsiveNames[newIdx]);
+    this.state = this.responsiveNames[newIdx];
+  }
 
   changeState(s) {
     this.ResponsiveStateEmitted.emit(s);
