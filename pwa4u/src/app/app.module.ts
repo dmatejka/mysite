bb35e8159e323955c428dce4ebc6f1fe6bfc60a4
dmatejka <matejka@xpression.sk>
2018-07-10 23:12:43 +0200

    swipe for responsive image to change state

--- pwa4u/src/app/app.module.ts
+++ pwa4u/src/app/app.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  HammerGestureConfig,
+  HAMMER_GESTURE_CONFIG,
+} from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import * as Hammer from 'hammerjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { NavItemComponent } from './navigation/nav-item/nav-item.component';
 import { NavigationComponent } from './navigation/navigation.component';
+
+export class MyHammerConfig extends HammerGestureConfig {
+  // overrides = <any>{
+  //   // override hammerjs default configuration
+  //   swipe: { direction: Hammer.DIRECTION_ALL },
+  //   // 'touch-action': 'pan-y'
+  // };
+
+  buildHammer(element: HTMLElement) {
+    const mc = new Hammer(element, {
+      touchAction: 'pan-y',
+    });
+
+    return mc;
+  }
+}
 
 @NgModule({
   declarations: [AppComponent, NavigationComponent, NavItemComponent],
@@ -16,7 +37,12 @@ import { NavigationComponent } from './navigation/navigation.component';
     HomeModule,
     // MyMatModules,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- pwa4u/src/app/home/responsive-picture/responsive-picture.component.ts
+++ pwa4u/src/app/home/responsive-picture/responsive-picture.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 
 type ResponsiveState = 'desktop' | 'mobile' | 'tablet';
 
@@ -9,8 +16,25 @@ type ResponsiveState = 'desktop' | 'mobile' | 'tablet';
 })
 export class ResponsivePictureComponent implements OnInit {
   @Input() resState: ResponsiveState = 'tablet';
+  @Output() ResponsiveStateEmitted = new EventEmitter();
+  responsiveNames: Array<ResponsiveState> = ['desktop', 'mobile', 'tablet'];
 
   constructor() {}
 
   ngOnInit() {}
+
+  @HostListener('swiperight', ['$event.type'])
+  @HostListener('swipeleft', ['$event.type'])
+  swipe(e) {
+    const currentIdx = this.responsiveNames.indexOf(this.resState);
+    let newIdx = currentIdx;
+    if (e === 'swiperight') {
+      newIdx = currentIdx - 1 >= 0 ? currentIdx - 1 : 2;
+    }
+    if (e === 'swipeleft') {
+      newIdx = currentIdx + 1 < 3 ? currentIdx + 1 : 0;
+    }
+    this.resState = this.responsiveNames[newIdx];
+    this.ResponsiveStateEmitted.emit(this.resState);
+  }
 }
